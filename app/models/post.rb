95556b9cde18757440738c10ab9ac47c1c9a4b76
NouranMahmoud <nouran.mhmoud@gmail.com>
2014-09-15 08:38:02 +0300

    Refined application's style

--- app/models/post.rb
+++ app/models/post.rb
@@ -18,6 +18,7 @@ class Post < ActiveRecord::Base
   end
 
   def self.tag_counts
-    Tag.select("tags.name, count(taggings.tag_id) as count").joins(:taggings).group("taggings.tag_id")
+    Tag.select("tags.id, tags.name,count(taggings.tag_id) as count").
+    joins(:taggings).group("taggings.tag_id, tags.id, tags.name")
   end
 end

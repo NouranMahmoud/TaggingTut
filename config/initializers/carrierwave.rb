5bfe99ab4fd3c234a6392f5be1f9dbffdc1d8f15
George Belanger <realtorgeorgeb@gmail.com>
2018-02-26 10:33:32 +0800

    secret configs updated

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     ENV["Access Key ID"],
-    aws_secret_access_key: ENV["Secret Access Key"],
+    aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
+    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
     region:                'ap-southeast-1'  
                           # required
     }

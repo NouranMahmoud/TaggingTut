c676cffa50dc960e6ca9253d3436ecf69d0a9161
George Belanger <realtorgeorgeb@gmail.com>
2018-02-26 12:47:36 +0800

    removed other attachments

--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -1,5 +1,5 @@
 class ReservationMailer < ApplicationMailer
-	
+
 	def booking_email(user)
 		@user= user
 	end
@@ -8,7 +8,6 @@ class ReservationMailer < ApplicationMailer
     @user = user
     @url  = 'https://ancient-headland-22384.herokuapp.com/'
     email_with_name = %("#{@user.name}" <#{@user.email}>)
-    mail(to: email_with_name, subject: 'Welcome to My Awesome Site')
-    attachments.inline['bb558ec7-1bcb-4585-a03b-bc540aa0b201.c10.jpg'] = File.read('/home/nextacademy/Desktop/bb558ec7-1bcb-4585-a03b-bc540aa0b201.c10.jpg')
+    mail(to: email_with_name, subject: 'Welcome to My Awesome Site'))
   end
 end
